0b7fd1e586cb415ba6d33c5b9d67a066781e6d04
Olyno <olyno.dev@gmail.com>
2021-04-07 00:28:52 +0200

    :tada: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import App from './App.svelte';
+import 'virtual:windi.css';
+import 'virtual:windi-devtools';
+
+const app = new App({
+  target: document.body
+})
+
+export default app;
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import svelte from '@sveltejs/vite-plugin-svelte';
+import WindiCSS from 'vite-plugin-windicss';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    WindiCSS({
+      scan: {
+        dirs: ['src'],
+        fileExtensions: ['svelte', 'ts'],
+      },
+    })
+  ]
+})
